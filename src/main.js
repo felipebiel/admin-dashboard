936b1f273deceec7efa5074191728a55395f2c8c
Felipe Biel <felipe.biel.souza@gmail.com>
2022-01-03 19:31:51 -0300

    feat: Registros de componentes universais

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ import App from './App.vue';
 // css global
 import '@/scss/core.scss';
 import router from './router';
+// registro de componentes
+import ComponentRegistration from '@/utils/component-registration';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+ComponentRegistration(app);
+
+app.use(router).mount('#app');
---
+++ src/utils/component-registration.js
@@ -0,0 +1,5 @@
+// Components
+import CardInfo from '@/components/custom/CardInfo';
+export default (app) => {
+    app.component('fb-card-info', CardInfo);
+};
