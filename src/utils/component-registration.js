4e322cff260c3bd3f40834fe77b8b88befce9c25
Felipe Biel <felipe.biel.souza@gmail.com>
2022-01-04 10:36:40 -0300

    feat: Registrando novos componentes universais

--- src/utils/component-registration.js
+++ src/utils/component-registration.js
@@ -1,5 +1,11 @@
 // Components
 import CardInfo from '@/components/custom/CardInfo';
+import CardComponent from '@/components/custom/CardComponent';
+import TitleBar from '@/components/custom/TitleBar';
+import ButtonComponent from '@/components/custom/ButtonComponent';
 export default (app) => {
     app.component('fb-card-info', CardInfo);
+    app.component('fb-card', CardComponent);
+    app.component('fb-title-bar', TitleBar);
+    app.component('fb-button', ButtonComponent);
 };
