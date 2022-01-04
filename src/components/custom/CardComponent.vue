44896b7ac480a109bb949bdbd36f6faf0251265d
Felipe Biel <felipe.biel.souza@gmail.com>
2022-01-04 10:35:05 -0300

    feat: Componente de card

---
+++ src/components/custom/CardComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.card {
+    padding-left: 2.5rem /* 40px */;
+    padding-right: 2.5rem /* 40px */;
+    padding-top: 2.75rem /* 44px */;
+    padding-bottom: 2.75rem /* 44px */;
+    box-shadow: 0 7px 25px rgba(0, 0, 0, 0.08);
+    border-radius: 20px;
+    background-color: $white;
+}
+</style>
